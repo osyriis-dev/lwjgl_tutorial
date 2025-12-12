753e878b31d8fed3982ae3fcf7c7812d9bff9753
veronica <vlivenoti@gmail.com>
2025-12-12 17:39:10 +0100

    Spostati metodi comuni a tutti i renderer nel MasterRenderer

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -4,6 +4,9 @@ import entities.Camera;
 import entities.Entity;
 import entities.Light;
 import models.TexturedModel;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Matrix4f;
 import shaders.StaticShader;
 
 import java.util.ArrayList;
@@ -13,18 +16,35 @@ import java.util.Map;
 
 public class MasterRenderer {
 
+    private Matrix4f projectionMatrix;
     private StaticShader shader = new StaticShader();
-    private Renderer renderer = new Renderer(shader);
+    private EntityRenderer entityRenderer;
 
     private Map<TexturedModel, List<Entity>> entities = new HashMap<>();
 
+    public MasterRenderer() {
+
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glCullFace(GL11.GL_BACK);
+        createProjectionMatrix();
+        entityRenderer = new EntityRenderer(shader, projectionMatrix);
+
+    }
+
+    //Field of view
+    private static final float FOV = 70;
+    //distanza al piano vicino
+    private static final float NEAR_PLANE = 0.1f;
+    //distanza al piano lontano
+    private static final float FAR_PLANE = 1000;
+
     public void render(Light sun, Camera camera) {
 
-        renderer.prepare();
+        prepare();
         shader.start();
         shader.loadLight(sun);
         shader.loadViewMatrix(camera);
-        renderer.render(entities);
+        entityRenderer.render(entities);
         shader.stop();
         entities.clear();
 
@@ -47,6 +67,31 @@ public class MasterRenderer {
     public void cleanUp() {
 
         shader.cleanUp();
+
+    }
+
+    public void prepare() {
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
+        GL11.glClearColor(0, 0, 0, 1);
+
+    }
+
+    private void createProjectionMatrix() {
+
+        float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
+        float y_scale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
+        float x_scale = y_scale / aspectRatio;
+        float frustum_length = FAR_PLANE - NEAR_PLANE;
+
+        projectionMatrix = new Matrix4f();
+        projectionMatrix.m00 = x_scale;
+        projectionMatrix.m11 = y_scale;
+        projectionMatrix.m22 = -((FAR_PLANE + NEAR_PLANE) / frustum_length);
+        projectionMatrix.m23 = -1;
+        projectionMatrix.m32 = -((2 * NEAR_PLANE + FAR_PLANE) / frustum_length);
+        projectionMatrix.m33 = 0;
 
     }
 }
